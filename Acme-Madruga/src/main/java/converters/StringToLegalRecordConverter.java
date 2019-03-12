edb5b7a053970a240a4ba2b509dd280abcb36f67
alexfdez17 <31543462+alexfdez17@users.noreply.github.com>
2019-03-12 10:02:08 +0100

    Converters

---
+++ Acme-Madruga/src/main/java/converters/StringToLegalRecordConverter.java
@@ -0,0 +1,39 @@
+
+package converters;
+
+import javax.transaction.Transactional;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import domain.LegalRecord;
+
+@Component
+@Transactional
+public class StringToLegalRecordConverter implements Converter<String, LegalRecord> {
+
+	@Autowired
+	private LegalRecordRepository	legalRecordRepository;
+
+
+	@Override
+	public LegalRecord convert(final String text) {
+		LegalRecord result;
+		int id;
+
+		try {
+			if (StringUtils.isEmpty(text))
+				result = null;
+			else {
+				id = Integer.valueOf(text);
+				result = this.legalRecordRepository.findOne(id);
+			}
+		} catch (final Throwable oops) {
+			throw new IllegalArgumentException(oops);
+		}
+		return result;
+	}
+
+}
---
+++ Acme-Madruga/src/main/java/converters/StringToPeriodRecordConverter.java
@@ -0,0 +1,39 @@
+
+package converters;
+
+import javax.transaction.Transactional;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+import domain.PeriodRecord;
+
+@Component
+@Transactional
+public class StringToPeriodRecordConverter implements Converter<String, PeriodRecord> {
+
+	@Autowired
+	private PeriodRecordRepository	periodRecordRepository;
+
+
+	@Override
+	public PeriodRecord convert(final String text) {
+		PeriodRecord result;
+		int id;
+
+		try {
+			if (StringUtils.isEmpty(text))
+				result = null;
+			else {
+				id = Integer.valueOf(text);
+				result = this.periodRecordRepository.findOne(id);
+			}
+		} catch (final Throwable oops) {
+			throw new IllegalArgumentException(oops);
+		}
+		return result;
+	}
+
+}

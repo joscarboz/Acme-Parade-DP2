f4e679fd14b3ddafa19a1cc87654dda7c5e7b41b
Jose <joscarboz@gmail.com>
2019-03-21 11:55:41 +0100

    fixed

--- Acme-Madruga/src/main/java/repositories/InceptionRecordRepository.java
+++ Acme-Madruga/src/main/java/repositories/InceptionRecordRepository.java
@@ -2,7 +2,6 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.InceptionRecord;
@@ -10,6 +9,4 @@ import domain.InceptionRecord;
 @Repository
 public interface InceptionRecordRepository extends JpaRepository<InceptionRecord, Integer> {
 
-	@Query("select i from InceptionRecordRecord i where i.id=?1")
-	InceptionRecord findOne(int id);
 }
--- Acme-Madruga/src/main/java/repositories/LegalRecordRepository.java
+++ Acme-Madruga/src/main/java/repositories/LegalRecordRepository.java
@@ -2,15 +2,11 @@
 package repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import domain.LegalRecord;
 
 @Repository
 public interface LegalRecordRepository extends JpaRepository<LegalRecord, Integer> {
-
-	@Query("select l from LegalRecord l where l.id=?1")
-	LegalRecord findOne(int id);
 
 }
